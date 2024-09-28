4654268ce9e8529de413b2c9df65f4beb4e37326
Edward Malte <eedwardmaltee.270@gmail.com>
2024-09-28 11:42:39 -0500

    CUCOL001-Initial module structure is defined.

---
+++ Milk-Collection-Clon/src/test/java/com/module/Milk_Collection_Clon/MilkCollectionClonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module.Milk_Collection_Clon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkCollectionClonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
