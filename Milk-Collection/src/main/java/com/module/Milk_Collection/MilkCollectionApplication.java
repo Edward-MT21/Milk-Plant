7060e9d000fd5305b1461176587f7373015fca7b
Edward Malte <eedwardmaltee.270@gmail.com>
2025-04-19 12:30:08 -0500

    [CUCOL001]: Update audit columns using Spring Data 26.

--- Milk-Collection/src/main/java/com/module/Milk_Collection/MilkCollectionApplication.java
+++ Milk-Collection/src/main/java/com/module/Milk_Collection/MilkCollectionApplication.java
@@ -2,8 +2,10 @@ package com.module.Milk_Collection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class MilkCollectionApplication {
 
 	public static void main(String[] args) {
---
+++ Milk-Collection/src/main/java/com/module/Milk_Collection/audit/AuditAwareImpl.java
@@ -0,0 +1,21 @@
+package com.module.Milk_Collection.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    /**
+     * Returns the current auditor of the application.
+     *
+     * @return the current auditor.
+     */
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("MILK_SUPPLIER_MS");
+    }
+
+}
