e1002653dbbae4e07328607a7b0ddcce67a10b50
Edward Malte <eedwardmaltee.270@gmail.com>
2025-05-12 18:57:11 -0500

    [CUCOL001]: Reading configurations using environment interface.

--- Financial-Management/src/main/java/com/module/Financial_Management/controllers/FinancialManagementController.java
+++ Financial-Management/src/main/java/com/module/Financial_Management/controllers/FinancialManagementController.java
@@ -1,5 +1,8 @@
 package com.module.Financial_Management.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/FinancialManagementController")
 public class FinancialManagementController {
 
+    @Value("${build.version}")
+    private String buildVersion;
+
+
     @GetMapping("/getGreeting")
     public String getGreeting() {
         return "Hello World since getGreeting FinancialManagement.";
+    }
+
+    @GetMapping("/getBuildVersion")
+    public ResponseEntity<String> getBuildVersion() {
+        return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
     }
 
 }
