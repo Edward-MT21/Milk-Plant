56e5d2573d2fb589678a145b228f5b7a0a561ad0
Edward Malte <eedwardmaltee.270@gmail.com>
2025-05-19 18:12:29 -0500

    [CUCOL001]: Refresh configurations at runtime using refresh actuator path 88.

--- Persons/src/main/java/com/persons/Persons/model/dtos/PersonsContactsDto.java
+++ Persons/src/main/java/com/persons/Persons/model/dtos/PersonsContactsDto.java
@@ -1,11 +1,18 @@
 package com.persons.Persons.model.dtos;
 
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "persons-contacts")
-public record PersonsContactsDto(String message, Map<String, String> contactDetails, List<Integer> onCallSupport) {
+@Getter
+@Setter
+public class PersonsContactsDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<Integer> onCallSupport;
 }
