7e07777082fc3f3f78d5aa7a5def3607d5b5b4c2
Edward Malte <eedwardmaltee.270@gmail.com>
2025-05-25 17:32:11 -0500

    [CUCM001]: Refactor coding of unnecessary objects from persons.

--- Milk-Collection/src/main/java/com/module/Milk_Collection/repositories/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.module.Milk_Collection.repositories;
-
-import com.module.Milk_Collection.model.entities.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
