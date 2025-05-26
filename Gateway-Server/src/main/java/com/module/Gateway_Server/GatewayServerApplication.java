fed2914ec55c1d464e5292f3e86b47cd62a5379c
Edward Malte <eedwardmaltee.270@gmail.com>
2025-05-26 17:53:14 -0500

    [CUCM001]: Demo of addResponseHeader filter 130..

--- Gateway-Server/src/main/java/com/module/Gateway_Server/GatewayServerApplication.java
+++ Gateway-Server/src/main/java/com/module/Gateway_Server/GatewayServerApplication.java
@@ -21,17 +21,17 @@ public class GatewayServerApplication {
 				.route(p -> p
 						.path("/milk-plant/milk-collection/**")
 						.filters( f -> f.rewritePath("/milk-plant/milk-collection/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time-LDT", LocalDateTime.now().toString()))
 						.uri("lb://MILK-COLLECTION"))
 				.route(p -> p
 						.path("/milk-plant/persons/**")
 						.filters( f -> f.rewritePath("/milk-plant/persons/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time-LDT", LocalDateTime.now().toString()))
 						.uri("lb://PERSONS"))
 				.route(p -> p
 						.path("/milk-plant/financial-management/**")
 						.filters( f -> f.rewritePath("/milk-plant/financial-management/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time-LDT", LocalDateTime.now().toString()))
 						.uri("lb://FINANCIAL-MANAGEMENT")).build();
 	}
 
