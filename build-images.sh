b5c76bb17ec42da21cd13faa8ec046b8c2fbc61d
Edward Malte <eedwardmaltee.270@gmail.com>
2025-06-01 18:39:25 -0500

    [CUCM001]: Implementing logging using Grafana, Loki & Alloy 162, 163, 164.

---
+++ build-images.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Directorios específicos donde se construirán las imágenes
+SERVICES=("Config-Server" "Eureka-Server" "Milk-Collection" "Persons" "Financial-Management" "Gateway-Server")  # Agrega aquí los nombres de los directorios
+
+# Recorre los directorios especificados
+for SERVICE in "${SERVICES[@]}"; do
+    if [ -d "$SERVICE" ]; then
+        echo "Construyendo imagen para: $SERVICE"
+        cd "$SERVICE" || exit 1
+        mvn compile jib:dockerBuild
+        cd ..
+    else
+        echo "El directorio $SERVICE no existe, omitiendo..."
+    fi
+done
+
+echo "Proceso completado 🚀"
