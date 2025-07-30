54144f661405514a33265a94f74bfc2762ccbc8d
Edward Malte <eedwardmaltee.270@gmail.com>
2025-07-30 14:45:37 -0500

    [CCMP]: Dependency inversion principle was adjusted.

--- Persons/src/main/java/com/persons/Persons/services/IPersonService.java
+++ Persons/src/main/java/com/persons/Persons/services/IPersonService.java
@@ -3,14 +3,20 @@ package com.persons.Persons.services;
 import com.persons.Persons.model.dtos.PersonInDto;
 import com.persons.Persons.model.dtos.PersonOutDto;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 public interface IPersonService {
+
     void createPerson(PersonInDto personInDto);
 
     void editPerson(PersonInDto personInDto);
 
     List<PersonOutDto> getAllPersons();
+
+    PersonOutDto fetchPersonById(Long personId);
 }
